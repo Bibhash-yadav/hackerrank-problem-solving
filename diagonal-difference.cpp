c026adb3deb514d4bbd55e8099e23569a8c9e84c
techxtor <techxtor472@gmail.com>
2022-04-02 20:54:58 +0545

    nothing

--- diagonal-difference.cpp
+++ diagonal-difference.cpp
@@ -8,7 +8,6 @@ vector<string> split(const string &);
 
 /*
  * Complete the 'diagonalDifference' function below.
- *
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
